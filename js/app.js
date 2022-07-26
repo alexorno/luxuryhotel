23c12054f9a72f423154246a2969e9510a492b11
alexorno <109001384+alexorno@users.noreply.github.com>
2022-07-26 18:22:59 +0200

    minor changes in bg

--- js/app.js
+++ js/app.js
@@ -1,113 +1,89 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', function () {
-    
-    const scrollDown = document.querySelector('.awelcomepage-scroll-inner');
-    const firstSlideScroll = document.querySelector('.roomtypes-inner h1') || document.querySelector('.facilities h1') || document.querySelector('.rooms h1');
-    
-    
-    
-    // roomDetailsName[0].addEventListener('click', function () {
-    //     roomDetail[0].classList.toggle('active');
-    // });
-    // roomDetailsName[1].addEventListener('click', function () {
-    //     roomDetail[1].classList.toggle('active');
-    // });
-    // roomDetailsName[2].addEventListener('click', function () {
-    //     roomDetail[2].classList.toggle('active');
-    // });
-      
 
-    scrollDown.addEventListener('click', function () {
-        firstSlideScroll.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
+  const scrollDown = document.querySelector('.awelcomepage-scroll-inner');
+  const firstSlideScroll = document.querySelector('.roomtypes-inner h1') || document.querySelector('.facilities h1') || document.querySelector('.rooms h1');
+
+  scrollDown.addEventListener('click', function () {
+    firstSlideScroll.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
     });
+  });
+// swiper
+  const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  //   animations
+  const tl = gsap.timeline({
+    defaults: {
+      ease: "power3.inout"
+    }
+  });
+  gsap.registerPlugin(ScrollTrigger);
+  ScrollTrigger.defaults({
+    toggleActions: "restart complete restart reset",
+    start: "top bottom",
+    end: "center center"
+  });
+  
+
+  tl.fromTo('.awelcomepage', {
+    x: '-500px',
+    opacity: '0',
+  }, {
+    x: '0px',
+    opacity: '1',
+    duration: 1,
+    stagger: 0.2
+  });
 
 
-    const swiper = new Swiper('.swiper', {
+  const roomTypesLeft = gsap.utils.toArray('.roomtypes-inner-card-left');
 
-        direction: 'horizontal',
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
+  roomTypesLeft.forEach(roomTypes => {
+    gsap.from(roomTypes, {
+      x: -200,
+      scrollTrigger: {
+        trigger: roomTypes,
+        scrub: 0,
 
-
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+      }
+    })
+  });
 
 
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
-
-
-    //   animations
-    const tl = gsap.timeline({
-        defaults: {
-            ease: "power3.inout"
-        }
-    });
-
-    gsap.registerPlugin(ScrollTrigger);
-
-    ScrollTrigger.defaults({
-        toggleActions: "restart complete restart reset",
-        start: "top bottom",
-        end: "center center"
-    });
-
-
-    tl.fromTo('.awelcomepage', {
-        x: '-500px',
-        opacity: '0',
+  gsap.timeline({
+      scrollTrigger: {
+        trigger: ".testimonials-inner h1"
+      }
+    })
+    .fromTo(".testimonials-inner h1 ,.testimonials-inner p ,.testimonials-inner p1", {
+      y: '-50',
+      opacity: 0
     }, {
-        x: '0px',
-        opacity: '1',
-        duration: 1,
-        stagger: 0.2
+      y: '0',
+      opacity: 1,
+      duration: 1.5,
     });
-
-
-
-
-    const roomTypesLeft = gsap.utils.toArray('.roomtypes-inner-card-left');
-
-    roomTypesLeft.forEach(roomTypes => {
-        gsap.from(roomTypes, {
-            x: -200,
-            scrollTrigger: {
-                trigger: roomTypes,
-                scrub: 0,
-
-            }
-        })
-    });
-
-
-    gsap.timeline({
-            scrollTrigger: {
-                trigger: ".testimonials-inner h1"
-            }
-        })
-        .fromTo(".testimonials-inner h1 ,.testimonials-inner p ,.testimonials-inner p1", {
-            y: '-50',
-            opacity: 0
-        }, {
-            y: '0',
-            opacity: 1,
-            duration: 1.5,
-        });
 
 });
-
